442a5cc12f3783c9ffa5c2e46a210ded0922a21d
RobynVD <vandeventerrobyn@gmail.com>
2017-05-30 13:43:54 +1000

    Implement truth table. Go mad that it doesn't output the correct result for the given example.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Introduction-To-AI
+
+## Links Used
+
+http://snipplr.com/view/56296/ai-forward-chaining-implementation-for-propositional-logic-horn-form-knowledge-bases/
+
+http://snipplr.com/view/56297/
+
+https://stackoverflow.com/a/10723228
+
+Textbook
+
+http://mrieppel.net/prog/truthtable.html 
+
+Lecture slides
--- com.example.IntroToAI/src/Inference/TruthTable.java
+++ com.example.IntroToAI/src/Inference/TruthTable.java
@@ -11,9 +11,11 @@ public class TruthTable {
 
     private String tell;
     private String ask;
+    private String string = "";
     private ArrayList<String> facts;
     private ArrayList<String> clauses;
     private ArrayList<String> unique;
+    private int truthCount = 0;
 
     public TruthTable(String tell, String ask) {
 
@@ -26,6 +28,7 @@ public class TruthTable {
         setup();
         getUnique();
         generate(unique.size());
+        System.out.println(truthCount());
     }
 
     private void setup() {
@@ -37,7 +40,7 @@ public class TruthTable {
                 clauses.add(string);
             }
             else {
-                facts.add(string);
+                facts.add(string.trim());
             }
         }
     }
@@ -81,7 +84,7 @@ public class TruthTable {
 
         // print headers
         for (String string: unique) {
-            System.out.print(string + "     ");
+            System.out.print(string + " ");
         }
 
         for (String string: clauses) {
@@ -100,9 +103,186 @@ public class TruthTable {
             while (s.length() != n) {
                 s = '0'+s;
             }
-            System.out.println(s + "   ");
+            System.out.println(formatTable(s));
+            string = "";
             count++;
         }
         System.out.println(count);
+    }
+
+    private String formatTable(String s) {
+
+        // find the count of unique symbols
+        int length = unique.size();
+
+        // pad the t/f values to line up with the headings
+        for (int i = 0; i < length; i++) {
+            string += s.substring(i, i+1);
+            string += " ";
+            if (unique.get(i).length() > 1) string += " ";
+        }
+
+        string += " ";
+
+        // validate each clause against the truth table
+        // validate each fact against the truth table
+        for (String clause: clauses) {
+            string += validateClause(clause, s);
+            for (int i = 0; i < clause.length(); i++) {
+                string += " ";
+            }
+        }
+
+        for (String fact: facts) {
+            string += validateFact(fact, s);
+            string += "       ";
+        }
+
+        validateAsk(string);
+
+        return string;
+    }
+
+    private String validateClause(String clause, String truthTable) {
+
+        // find the clause position
+        int clausePosition = clauses.indexOf(clause);
+        String string = "";
+
+        // split the clause
+        // find the indexes of each symbol
+        // find the second symbol value
+        String[] splitClause = clauses.get(clausePosition).split("=>");
+        int firstSymbolIndex = unique.indexOf(splitClause[0].trim());
+        int secondSymbolIndex = unique.indexOf(splitClause[1].trim());
+        int secondSymbol = Character.getNumericValue(truthTable.charAt(secondSymbolIndex));
+
+        // split the premise
+        if (splitClause[0].contains("&")) {
+            String[] splitPremise = splitClause[0].split("&");
+            int firstPremiseIndex = unique.indexOf(splitPremise[0]);
+            int secondPremiseIndex = unique.indexOf(splitPremise[1]);
+            int firstPremiseSymbol = Character.getNumericValue(firstPremiseIndex);
+            int secondPremiseSymbol = Character.getNumericValue(secondPremiseIndex);
+
+            // false
+            if (((firstPremiseSymbol == 1) && (secondPremiseSymbol == 1)) && secondSymbol == 0) {
+                string += "0    ";
+            }
+            // true
+            else {
+                string += "1    ";
+            }
+        }
+        else {
+            int firstSymbol = Character.getNumericValue(truthTable.charAt(firstSymbolIndex));
+
+            // false
+            if ((firstSymbol == 1) && (secondSymbol == 0)) {
+                string += "0    ";
+            }
+            // true
+            else {
+                string += "1    ";
+            }
+        }
+
+        return string;
+    }
+
+    private String validateFact(String fact, String truthTable) {
+
+        int factPosition = unique.indexOf(fact.trim());
+
+        // return the symbol value of the fact
+        return String.valueOf(truthTable.charAt(factPosition));
+    }
+
+    private void validateAsk(String string) {
+        String table = string.replaceAll("\\s+","");
+        String[] completeTable = table.split("");
+        ArrayList<String> agenda = new ArrayList<>();
+        ArrayList<String> searched = new ArrayList<>();
+        agenda.add(ask);
+        int askClauseIndex = -1;
+        Boolean keepSearching = true;
+        String clause = null;
+
+        while (keepSearching) {
+
+            if (agenda.size() > 0) {
+                searched.add(agenda.get(0));
+                clause = agenda.remove(0);
+            }
+
+            // we are at the end and have proved the original ask
+            if (!clauses.contains(clause.trim()) && facts.contains(clause.trim())) {
+
+                int trueSymbols = 0;
+                for (String symbol: searched) {
+                    if (clauses.contains(symbol.trim())) {
+                        if (Integer.parseInt(completeTable[unique.size() + clauses.indexOf(symbol.trim())]) == 1) trueSymbols++;
+                    }
+                    else {
+                        if (Integer.parseInt(completeTable[unique.size() + clauses.size() + facts.indexOf(clause.trim())]) == 1) trueSymbols++;
+                    }
+                }
+                if (trueSymbols == searched.size()) {
+                    truthCount++;
+                }
+                return;
+            }
+
+            // find the clause with the symbol we are trying to infer
+            for (int i = 0; i < clauses.size(); i++) {
+                if (clauses.get(i).contains(clause.trim())) {
+                    if (clauses.get(i).split("=>")[1].contains(clause.trim())) {
+                        askClauseIndex = i;
+                    }
+                }
+            }
+
+            // find the index in the filled out table that corresponds to the ask's clause
+            if (askClauseIndex >= 0) {
+                int clauseValue = Integer.parseInt(completeTable[unique.size() + askClauseIndex]);
+
+                // if the clause is true, we can proceed
+                if (clauseValue == 1) {
+                    String[] splitClause = clauses.get(askClauseIndex).split("=>");
+
+                    // check if the clause is inferring our ask
+                    if (splitClause[1].contains(clause.trim())) {
+                        if (splitClause[0].contains("&")) {
+                            String[] splitPremise = splitClause[0].split("&");
+                            for (String premise: splitPremise) {
+                                agenda.add(premise);
+                            }
+                        } else {
+                            agenda.add(splitClause[0]);
+                        }
+                    }
+                }
+                else {
+                    keepSearching = false;
+                }
+            }
+            else {
+                keepSearching = false;
+            }
+        }
+    }
+
+    private String truthCount() {
+
+        String string = "";
+
+        if (truthCount > 0) {
+            string += "YES: " + truthCount;
+        }
+        else {
+            string += "NO";
+        }
+
+        return string;
     }
 }
